ca90b4a0f7475b43502ba3d2c3a52ca67095f563
k1017694 <aparna.pathak@krones.com>
2024-10-15 18:45:41 +0530

    NGRX CHANGES

--- Task-Buddy/task-buddy-app/src/main.ts
+++ Task-Buddy/task-buddy-app/src/main.ts
@@ -1,16 +1,6 @@
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app/app.routes';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    BrowserModule,
-    BrowserAnimationsModule,
-    provideHttpClient(),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
