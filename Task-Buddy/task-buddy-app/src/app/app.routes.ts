9963236eab7118d01596bdd76a35625ef4ed700a
k1017694 <aparna.pathak@krones.com>
2024-10-15 18:41:35 +0530

    ngrx changes

--- Task-Buddy/task-buddy-app/src/app/app.routes.ts
+++ Task-Buddy/task-buddy-app/src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TaskSchedulerComponent } from './task-scheduler/task-scheduler.component';
-import { CounterComponent } from './counter/counter/counter.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'task', component: TaskSchedulerComponent },
-  { path: 'counter', component: CounterComponent },
-  { path: '**', redirectTo: 'dashboard' },
-];
+export const routes: Routes = [];
